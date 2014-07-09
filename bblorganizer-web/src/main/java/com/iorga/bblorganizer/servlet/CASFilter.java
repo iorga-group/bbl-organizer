ee36504f3ca49a17084b62d6e27d78f7ffb343fa
Anthony OGIER <aogier@iorga.com>
2014-07-09 13:21:23 +0200

    Adding security

--- bblorganizer-web/src/main/java/com/iorga/bblorganizer/servlet/CASFilter.java
+++ bblorganizer-web/src/main/java/com/iorga/bblorganizer/servlet/CASFilter.java
@@ -1,5 +1,7 @@
 package com.iorga.bblorganizer.servlet;
 
+import java.io.IOException;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletResponse;
@@ -12,12 +14,21 @@ public class CASFilter extends AbstractAutoLogOnceCASFilter {
 
 	@Override
 	protected boolean tryLogin(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-		return false;
+		return true;
 	}
 
 	@Override
 	protected boolean haveToCasFilter(HttpServletRequest request, ServletResponse response, FilterChain filterChain) {
 		return true;
+	}
+
+	@Override
+	protected void doAfterCASFiltersAndLoggedOnce(HttpServletRequest httpRequest, HttpServletResponse httpResponse, FilterChain chain, boolean loggedIn) throws IOException, ServletException {
+		if (loggedIn) {
+			super.doAfterCASFiltersAndLoggedOnce(httpRequest, httpResponse, chain, loggedIn);
+		} else {
+			httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "You must log in.");
+		}
 	}
 
 }
